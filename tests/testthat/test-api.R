b27919fb79d0ac2eda08d8bb9f57d8663e964d4f
Hammu144 <hamzamunir00@gmail.com>
2022-10-03 23:37:01 +0200

    test and shiny last updation

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -2,13 +2,19 @@ testthat:: test_that("not a character", {
   expect_error(fetch_qr(TRUE,"150*150"))
   expect_error(fetch_qr(TRUE,150*150))
   expect_error(fetch_qr(23,150*150))
+  expect_error(fetch_qr(23,"150*150"))
+  expect_error(fetch_qr(hama,150*150))
 
 })
 
 
-testthat:: test_that("not a character", {
-  expect_success(expect_type(fetch_qr("hamza",150*150), "raw"))
-  expect_success(expect_type(fetch_qr("hamza",200*200), "raw"))
+testthat:: test_that("expecting value", {
+  expect_success(expect_type(fetch_qr("ham",150*150), "raw"))
+  expect_success(expect_type(fetch_qr("ha",200*200), "raw"))
+  expect_success(expect_type(fetch_qr("haa",100*100), "raw"))
+  expect_success(expect_type(fetch_qr("hza",200*200), "raw"))
+  expect_success(expect_type(fetch_qr("al",100*100), "raw"))
+
 
 })
 
